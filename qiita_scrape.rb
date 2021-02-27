14ce765dece9b7de7ce655515f18b6c1ad14f63a
t0ottio13 <adsoption104@gmail.com>
2021-02-27 17:40:52 +0900

    create qiita_scraper

---
+++ qiita_scrape.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+
+url = 'https://qiita.com/search?q=ruby'
+
+charset = nil
+titles = []
+
+html = open(url) do |f|
+    charset = f.charset
+    f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('//h1[@class="searchResult_itemTitle"]').each do |node|
+    title = node.css('a').inner_text
+    titles.push(title)
+end
+
+csv = CSV.open("title.csv", "w")
+csv << titles
